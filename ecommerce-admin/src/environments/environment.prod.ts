a55a069d4be0e0a035e5f94fa1da738638e0d936
Suraj Rajbhar <suraj.rajbhar.tech@gmail.com>
2025-07-13 21:32:04 +0530

    new commit

--- ecommerce-admin/src/environments/environment.prod.ts
+++ ecommerce-admin/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ecommerce-app-4bnj.onrender.com/api', // replace with actual Render URL
+  apiUrl: 'https://ecommerce-backend-sqh8.onrender.com',
 };
--- ecommerce-admin/src/environments/environment.ts
+++ ecommerce-admin/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://ecommerce-app-4bnj.onrender.com/api',
+  apiUrl: 'https://ecommerce-backend-sqh8.onrender.com',
 };
